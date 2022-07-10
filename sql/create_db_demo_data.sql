241f7dacae724f2fc703c8730922bcacfad3cdc8
GGOx94 <mustbehax@hotmail.fr>
2022-07-10 17:37:43 +0200

    updated sql demo script

--- sql/create_db_demo_data.sql
+++ sql/create_db_demo_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 27 juin 2022 à 15:29
+-- Généré le : dim. 10 juil. 2022 à 17:21
 -- Version du serveur : 10.4.24-MariaDB
 -- Version de PHP : 8.1.4
 
@@ -12,8 +12,10 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 --
--- Base de données : `p5phpblog`
+-- Base de données : `p5phpblogDEMO`
 --
+CREATE DATABASE IF NOT EXISTS `p5phpblogDEMO` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `p5phpblogDEMO`;
 
 -- --------------------------------------------------------
 
@@ -35,8 +37,9 @@ CREATE TABLE `comment` (
 --
 
 INSERT INTO `comment` (`id`, `fk_post_id`, `fk_user_name`, `fk_comment_status`, `content`, `creation_date`) VALUES
-(1, 1, 'Jane123', 'approved', 'This is Jane and I find this simply delightful !', '2022-05-08 13:46:59'),
-(2, 1, 'John Doe', 'waiting_approval', 'This is John and this post is so bad I lost all hope in humanity.', '2022-05-08 13:48:20');
+(44, 13, 'Jane123', 'approved', 'Ce post a changé ma vie.\r\nMerci pour tout.', '2022-07-10 17:10:03'),
+(45, 13, 'John Doe', 'waiting_approval', 'Tout ceci était fort peu inspiré !\r\nJ&#039;ajouterai que : \r\n\r\nQuisque ac euismod neque. Donec sed metus ac sem eleifend suscipit. Aliquam ligula velit, laoreet vel diam et, tristique dictum sapien. Proin varius, erat a efficitur gravida, felis eros sollicitudin libero, quis laoreet turpis neque ac ipsum.', '2022-07-10 17:11:36'),
+(46, 14, 'admin_2', 'approved', 'Je ne connais pas cet auteur, mais il m&#039;a l&#039;air fort intelligent.', '2022-07-10 17:15:44');
 
 -- --------------------------------------------------------
 
@@ -77,7 +80,8 @@ CREATE TABLE `post` (
 --
 
 INSERT INTO `post` (`id`, `fk_user_name`, `title`, `head`, `content`, `creation_date`, `modification_date`) VALUES
-(1, 'ggo', 'Premier post par GGO !', 'Ceci est le chapo du post (Lorem ipsum dolor sit amet, consectetur adipiscing elit.)', 'Le contenu du post.\r\nQuisque mollis pellentesque orci eu laoreet. Maecenas in enim libero. Nunc cursus erat hendrerit massa ornare, vel faucibus justo molestie. Sed eget nulla a sapien volutpat rutrum venenatis ut sem. Nulla a nisi sit amet lectus auctor venenatis et non nisi. \r\n\r\nEtiam at varius purus. Phasellus lectus neque, mattis quis tempus et, gravida non mauris. Nullam pellentesque feugiat gravida. Nulla at cursus purus, ac venenatis turpis. Phasellus dictum dolor sit amet libero tincidunt, quis laoreet magna posuere. \r\n\r\nSuspendisse potenti. Sed vel ligula iaculis, cursus felis at, sollicitudin nibh. Aenean varius placerat iaculis. Phasellus fringilla lacinia odio eget congue.', '2022-05-08 13:44:07', NULL);
+(13, 'ggo', 'Premier Post par GGO', 'Ceci est un chapô...\r\nTrès inspiré.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec urna ut urna congue posuere. Pellentesque lacinia finibus viverra. Quisque vel libero vitae libero condimentum pulvinar id semper tellus. Mauris eu molestie ex. Quisque quis sem nec libero semper fermentum. Aliquam lacinia, libero vitae dignissim ornare, ipsum elit blandit leo, placerat faucibus nisi quam vel neque. Aenean scelerisque velit vitae diam vehicula tempus. Duis et accumsan turpis.\r\n\r\nModification !\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec urna ut urna congue posuere. Pellentesque lacinia finibus viverra. Quisque vel libero vitae libero condimentum pulvinar id semper tellus. Mauris eu molestie ex. Quisque quis sem nec libero semper fermentum. \r\n\r\nAliquam lacinia, libero vitae dignissim ornare, ipsum elit blandit leo, placerat faucibus nisi quam vel neque. Aenean scelerisque velit vitae diam vehicula tempus. Duis et accumsan turpis.', '2022-07-10 17:09:16', '2022-07-10 17:16:20'),
+(14, 'admin_2', 'Admin2 a son mot à dire...', 'Avec un chapô de qualité.', 'Et un contenu qui coupe le souffle :\r\n- Donec fringilla nec tortor sed mattis. Donec molestie mauris mi, eget aliquet velit pulvinar eu. Phasellus ac orci scelerisque, bibendum est a, tempor dolor. Vivamus id felis elit. Proin dui metus, luctus nec aliquam ac, pharetra nec turpis. Fusce vestibulum vel massa vitae auctor. Suspendisse vitae erat a risus eleifend mollis. Suspendisse cursus sollicitudin odio at interdum. Duis libero nunc, tristique in arcu at, ultricies aliquet nisl. Nulla mattis id tortor non gravida.\r\n\r\nDe plus :\r\n- Sed et tincidunt dolor. Suspendisse at nunc quis turpis gravida varius. Morbi imperdiet augue vel magna ornare, non ullamcorper turpis scelerisque. Vestibulum non neque lacinia, facilisis nisl eget, blandit nisl. Aenean sed est id metus feugiat tristique eu vitae lorem. Duis sed lacinia lacus. Nulla ut lacus ac ante pharetra dignissim vitae ut tortor. Aliquam posuere blandit dolor, non ullamcorper turpis viverra quis. Phasellus sed ante sed eros bibendum dapibus. \r\n\r\n- Vivamus quis euismod nisi. Morbi commodo pulvinar lectus sed vehicula. Nunc non mauris vitae turpis varius eleifend in volutpat arcu. Donec venenatis enim fermentum dolor lacinia lobortis. Suspendisse vestibulum est a ex elementum fringilla.', '2022-07-10 17:14:41', NULL);
 
 -- --------------------------------------------------------
 
@@ -100,14 +104,10 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`name`, `email`, `password`, `fk_user_status`, `token`, `expire_date`) VALUES
 ('admin_2', 'admin_2@p5phpblog.net', '202cb962ac59075b964b07152d234b70', 'admin', NULL, NULL),
-('demo', 'demo@demo.fr', '62cc2d8b4bf2d8728120d052163a77df', 'visitor', NULL, NULL),
-('demo2', 'demo2@demo.fr', '202cb962ac59075b964b07152d234b70', 'visitor', NULL, NULL),
-('demo3', 'demo3@fr.fr', '202cb962ac59075b964b07152d234b70', 'visitor', NULL, NULL),
-('eee', 'ee@e.ee', '202cb962ac59075b964b07152d234b70', 'signing_up', '37092d2291c8e18367dd35bcf68652f6d2b670c6a3061981bcc0ab90eb836853', '2022-06-16 17:33:24'),
+('Do Doe', 'do@doe.do', '202cb962ac59075b964b07152d234b70', 'banned', '43ce44b0ba23cd1de959e6c350c08eb01eb0afc1f2821d9f89f5aa98d630fa78', '2022-07-12 17:18:05'),
 ('ggo', 'ggo@p5phpblog.net', '202cb962ac59075b964b07152d234b70', 'admin', NULL, NULL),
 ('Jane123', 'jane_doe@gmail.com', '202cb962ac59075b964b07152d234b70', 'visitor', NULL, NULL),
-('John Doe', 'jdoe@hotmail.fr', '202cb962ac59075b964b07152d234b70', 'visitor', NULL, NULL),
-('nouveaugars', 'ng@ng.ng', '202cb962ac59075b964b07152d234b70', 'visitor', '48bfe8d17670b1a8ddaa9de6e98fee1ec2e788e6bb78f983d3b417e7888efa96', '2022-06-22 17:13:37');
+('John Doe', 'jdoe@hotmail.fr', '202cb962ac59075b964b07152d234b70', 'visitor', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -125,6 +125,7 @@ CREATE TABLE `user_status` (
 
 INSERT INTO `user_status` (`label`) VALUES
 ('admin'),
+('banned'),
 ('signing_up'),
 ('visitor');
 
@@ -176,13 +177,13 @@ ALTER TABLE `user_status`
 -- AUTO_INCREMENT pour la table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Contraintes pour les tables déchargées
